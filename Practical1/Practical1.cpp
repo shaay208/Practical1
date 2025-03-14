fc4a83bbec9032edc3dc2511f2f200450a6102f2
Shalini <shaay208@mymail.unisa.edu.au>
2025-03-15 00:22:52 +1030

    first commit

---
+++ Practical1/Practical1.cpp
@@ -0,0 +1,12 @@
+﻿// Practical1.cpp : Defines the entry point for the application.
+//
+
+#include "Practical1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

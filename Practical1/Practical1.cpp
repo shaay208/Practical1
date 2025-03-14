e88c52b58e3cce6ae3defe9272425b59fa58361a
Shalini <shaay208@mymail.unisa.edu.au>
2025-03-15 01:03:18 +1030

    Completed activity 1

--- Practical1/Practical1.cpp
+++ Practical1/Practical1.cpp
@@ -10,25 +10,48 @@ void function2(int*);
 int main() {
     int number = 10;
     /* C1. display the contents of number to the screen */
-    /* C2. display the address of variable number to the screen */
-    /* C3. call function1 */
-    /* C4. display the contents of number to the screen */
+    std::cout << "'number' equals to " << number << std::endl;
 
+    /* C2. display the address of variable number to the screen */
+    std::cout << "The address of 'number' is " << &number << std::endl;
+    
+    /* C3. call function1 */
+    function1(number);
+    
+    /* C4. display the contents of number to the screen */
+    std::cout << "After calling 'function', 'number' is now equal to " << number << std::endl;
+    
     /* C5. call function2 */
+    function2(&number);
+    
     /* C6. display the contents of number to the screen */
+    std::cout << "After calling 'function2', 'number' is now equal to " << number << std::endl;
+    
     return 0;
 }
 void function1(int number)
 {
     /* C7. increment number by one */
+    number++;
+
     /* C8. display the contents of number to the screen (should be 11) */
+    std::cout << "In 'function1`, `number' is now equal to " << number << std::endl;
+
     /* C9. display the address of number to the screen */
+    std::cout << "In 'function1', the address of 'number' is " << &number << std::endl;
 }
 void function2(int* numberPtr)
 {
     /* C10. display the address of number to the screen (should be the same
     address as you displayed first in main) */
+    std::cout << "In 'function2', the value of 'numberPtr' (the address of 'number') is " << numberPtr << std::endl;
+
     /* C11. display the value that the pointer numberPtr points to */
+    std::cout << "In 'function2', the value of 'number' is " << *numberPtr << std::endl;
+
     /* C12. increment the value stored in number */
+    (*numberPtr)++;
+
     /* C13. display the contents of number to the screen */
+    std::cout << "In 'function2', the value of 'number' is now " << *numberPtr << std::endl;
 }
